c810d2aa344b13f4a90ef0350ad934f68e55d292
Camilovelez1 <camilo95022@gmail.com>
2024-08-29 22:04:24 -0500

    add index

--- challenges/views.py
+++ challenges/views.py
@@ -1,40 +1,60 @@
-import calendar
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
-from django.http import HttpResponse, HttpResponseNotFound
+monthly_challenges_dict: dict[str, str] = {
+    "january": "Eat no meat the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat the entire month!",
+    "may": "Eat no meat the entire month!",
+    "june": "Eat no meat the entire month!",
+    "july": "Eat no meat the entire month!",
+    "august": "Eat no meat the entire month!",
+    "september": "Since September it feels like December is coming!",
+    "october": "Eat no meat the entire month!",
+    "november": "Eat no meat the entire month!",
+    "december": "Eat no meat the entire month!",
+}
+
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges_dict.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f'<li><a href="{month_path}">{capitalized_month}</a></li>'
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
 
 
 def monthly_challenge_by_number(request, month):
-    month_name = str(calendar.month_name[month]).lower()
-    challenge_text = monthly_challenges(request, month_name)
-    return HttpResponseNotFound(challenge_text)
-
-
-def monthly_challenges(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Eat no met the entire month!"
-    elif month == "february":
-        challenge_text = "Walk for at least 20 minutes every day!"
-    elif month == "march":
-        challenge_text = "Learn Django for at least 20 minutes every day"
-    elif month == "april":
-        challenge_text = "Eat no met the entire month4!"
-    elif month == "may":
-        challenge_text = "Eat no met the entire month5!"
-    elif month == "june":
-        challenge_text = "Eat no met the entire month6!"
-    elif month == "july":
-        challenge_text = "Eat no met the entire month7!"
-    elif month == "august":
-        challenge_text = "Eat no met the entire month8!"
-    elif month == "september":
-        challenge_text = "Eat no met the entire month9!"
-    elif month == "october":
-        challenge_text = "Eat no met the entire month10!"
-    elif month == "november":
-        challenge_text = "Eat no met the entire month11!"
-    elif month == "december":
-        challenge_text = "Eat no met the entire month12!"
+    months = list(monthly_challenges_dict.keys())
+    if 1 <= month <= 12:
+        redirect_month = months[month - 1]
+        redireth_path = reverse(
+            "month-challenge", args=[redirect_month]
+        )  # /challenges/january
+        return HttpResponseRedirect(redireth_path)
     else:
-        return HttpResponseNotFound("This month is no supported!")
-    return HttpResponse(challenge_text)
+        return HttpResponseNotFound("Invalid month number!")
+
+
+# def monthly_challenge_by_number(request, month):
+#    if 1 <= month <= 12:
+#        month_name = calendar.month_name[month].lower()
+#        challenge_text = monthly_challenges_dict.get(month_name)
+#        return HttpResponse(challenge_text)
+#    else:
+#        return HttpResponseNotFound("This month is not supported!")
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges_dict[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
+    except KeyError:
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
